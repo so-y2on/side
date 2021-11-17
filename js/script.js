6d3f9742f284f5e14c8b8582da978e8787535464
soy2on <so-y2on>
2021-11-17 18:30:55 +0900

    6:30

--- js/script.js
+++ js/script.js
@@ -2,12 +2,58 @@ var selectedRow = null;
 
 const u = 'http://chifuyu74.shop/post';
 
-async function onList() {
-  var res = await fetch(`${u}`, { method: 'GET' }).then((res) => res.json());
+var globals = [];
 
-  var posts = res.list;
+function onRead(postId) {
+  var currentPost = globals[postId - 1];
+  var currentH1 = document.querySelector('.title');
+  console.log(currentH1, currentPost);
+  // currentH1.innerHTML = currentPost.title;
+  currentH1.textContent = currentPost.title;
+
+  var currentUpdated = document.querySelector('p.updatedAt');
+  currentUpdated.innerHTML = currentPost.updatedAt;
+
+  var currentContent = document.querySelector('p.content');
+  currentContent.innerHTML = currentPost.content;
 }
 
+async function onList() {
+  var res = await fetch(`${u}`, { method: 'GET' }).then((res) => res.json());
+  globals = res.list;
+  var posts = res.list;
+
+  var tbody = document.querySelector('#listAll > tbody');
+
+  for (let i = 0; i < posts.length; i++) {
+    var id = posts[i].id;
+
+    var tr = document.createElement('tr');
+    tr.classList.add(`post_${id}`);
+    tr.style.cursor = 'pointer';
+    tr.addEventListener('click', function (e) {
+      onRead(id);
+    });
+
+    var idTd = document.createElement('td');
+    idTd.innerText = id;
+    tr.appendChild(idTd);
+
+    var authorTd = document.createElement('td');
+    authorTd.innerText = 'somebody';
+    tr.appendChild(authorTd);
+
+    var contentTd = document.createElement('td');
+    contentTd.innerText = posts[i].content;
+    tr.appendChild(contentTd);
+
+    var updatedTd = document.createElement('td');
+    updatedTd.innerText = posts[i].updatedAt;
+    tr.appendChild(updatedTd);
+
+    tbody.appendChild(tr);
+  }
+}
 onList();
 
 function onFormSubmit() {
@@ -17,6 +63,7 @@ function onFormSubmit() {
   else updateRecord(formData);
   resetForm();
 }
+
 function readFormData() {
   var formData = {};
   formData['title'] = document.getElementById('title').value;
@@ -24,6 +71,7 @@ function readFormData() {
   formData['content'] = document.getElementById('content').value;
   return formData;
 }
+
 function insertNewRecord(data) {
   var table = document.getElementById('listAll').getElementsByTagName('tbody')[0];
   var newRow = table.insertRow(table.length);
@@ -39,23 +87,27 @@ function insertNewRecord(data) {
   cell4.innerHTML = `<a onClick="onEdit(this)">수정</a>
                      <a onClick="onDelete(this)">삭제</a>`;
 }
+
 function resetForm() {
   document.getElementById('title').value = '';
   document.getElementById('writer').value = '';
   document.getElementById('content').value = '';
   selectedRow = null;
 }
+
 function onEdit(td) {
   selectedRow = td.parentElement.parentElement;
   document.getElementById('title').value = selectedRow.cells[0].innerHTML;
   document.getElementById('writer').value = selectedRow.cells[1].innerHTML;
   document.getElementById('content').value = selectedRow.cells[2].innerHTML;
 }
+
 function updateRecord(formData) {
   selectedRow.cells[0].innerHTML = formData.title;
   selectedRow.cells[1].innerHTML = formData.writer;
   selectedRow.cells[2].innerHTML = formData.content;
 }
+
 function onDelete(td) {
   if (confirm('정말 삭제하시겠습니까?')) {
     row = td.parentElement.parentElement;
